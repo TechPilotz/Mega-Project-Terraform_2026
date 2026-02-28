f8c7e5e97cdb5cc450b0e46415b1f37e86890b06
TechPilotz <mr.techpilotz@gmail.com>
2026-02-28 15:55:56 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,15 +1,24 @@
 output "cluster_id" {
-  value = aws_eks_cluster.devopsshack.id
+  description = "The ID of the Techpilotz EKS Cluster"
+  value       = aws_eks_cluster.techpilotz.id
 }
 
 output "node_group_id" {
-  value = aws_eks_node_group.devopsshack.id
+  description = "The ID of the Techpilotz EKS Node Group"
+  value       = aws_eks_node_group.techpilotz.id
 }
 
 output "vpc_id" {
-  value = aws_vpc.devopsshack_vpc.id
+  description = "The ID of the Techpilotz VPC"
+  value       = aws_vpc.techpilotz_vpc.id
 }
 
 output "subnet_ids" {
-  value = aws_subnet.devopsshack_subnet[*].id
+  description = "The IDs of the Techpilotz subnets"
+  value       = aws_subnet.techpilotz_subnet[*].id
+}
+
+output "cluster_endpoint" {
+  description = "The endpoint for your Techpilotz EKS Kubernetes API"
+  value       = aws_eks_cluster.techpilotz.endpoint
 }
